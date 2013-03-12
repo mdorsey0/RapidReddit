380025c6b620b02f51bc0d3a1ab6c54bc060c764
Mike Dorsey <mike.dorsey@gmail.com>
2013-03-12 01:44:05 -0400

    Integrated with Google Analytics

--- background.js
+++ background.js
@@ -11,3 +11,19 @@ chrome.storage.sync.get({
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.create({url: 'rr.html'});
 });
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-39205973-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
